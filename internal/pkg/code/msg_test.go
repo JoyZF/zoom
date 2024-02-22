473e3f1cf9cc053c5de312a9a75d3fab3b055c65
Joy <1003818507@qq.com>
2024-02-22 14:18:08 +0800

    fix: msg test

--- internal/pkg/code/msg_test.go
+++ internal/pkg/code/msg_test.go
@@ -6,12 +6,13 @@ package code
 
 import (
 	"fmt"
+	"github.com/JoyZF/zoom/global"
 	"testing"
 
 	"github.com/JoyZF/errors"
 )
 
 func TestGetMsg(t *testing.T) {
-	err := errors.WithCode(SystemError, GetMsg(SystemError, EN_US))
+	err := errors.WithCode(SystemError, GetMsg(SystemError, global.EN_US))
 	fmt.Println(fmt.Sprintf("%+v", err))
 }
